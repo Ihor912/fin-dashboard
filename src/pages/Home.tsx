0de6de411343bee5f349799080b0d7b04a35c94d
Ihor Khomiak <igor.homyak@gmail.com>
2024-07-02 01:24:23 +0200

    implement basic store

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Counter from "../features/counter/Counter";
 
 const Home: React.FC = () => {
   return (
     <div>
       <h1>Home Page</h1>
       <p>Welcome to the home page!</p>
+      <Counter />
     </div>
   );
 };
